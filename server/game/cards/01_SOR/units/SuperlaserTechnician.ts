16206a6052103e7fec130bc42c0f2ab9049261dd
abronico <adbronico92@gmail.com>
2024-09-22 18:13:18 -0400

    Revert "Update Superlaser Technician to use new ResourceCardSystem"
    This reverts commit ff042e16802eedf3c27f515d3e7e032d616827e8.

--- server/game/cards/01_SOR/units/SuperlaserTechnician.ts
+++ server/game/cards/01_SOR/units/SuperlaserTechnician.ts
@@ -1,5 +1,6 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { Location } from '../../../core/Constants';
 
 export default class SuperlaserTechnician extends NonLeaderUnitCard {
     protected override getImplementationId() {
@@ -16,7 +17,7 @@ export default class SuperlaserTechnician extends NonLeaderUnitCard {
             //TODO: figure out how to change this from sequential to simultaneous while keeing the ready effect
             immediateEffect: AbilityHelper.immediateEffects.sequential([
                 //TODO: create a MoveCardToResourceSystem and accompanying function in immediateEffects
-                AbilityHelper.immediateEffects.resourceCard((context) => ({ target: context.source})),
+                AbilityHelper.immediateEffects.moveCard((context) => ({ target: context.source, destination: Location.Resource })),
                 AbilityHelper.immediateEffects.ready((context) => ({ target: context.source }))])
         });
     }
