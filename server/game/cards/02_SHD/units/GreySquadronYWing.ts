2c41fb4317a8c19d4becd261876561439dac50dd
Valentin Jubert <val_jub@live.fr>
2024-10-10 02:23:53 +0200

    grey squadron y wing

---
+++ server/game/cards/02_SHD/units/GreySquadronYWing.ts
@@ -0,0 +1,25 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { RelativePlayer } from '../../../core/Constants';
+
+export default class GreySquadronYWing extends NonLeaderUnitCard {
+    protected override getImplementationId () {
+        return {
+            id: '9472541076',
+            internalName: 'grey-squadron-ywing'
+        };
+    }
+
+    public override setupCardAbilities () {
+        this.addOnAttackAbility({
+            title: 'Deal 2 damage to a ground unit if Koska Reeves is upgraded',
+            targetResolver: {
+                choosingPlayer: RelativePlayer.Opponent,
+                controller: RelativePlayer.Opponent,
+                immediateEffect: AbilityHelper.immediateEffects.damage({ optional: true, amount: 2 })
+            },
+        })
+    }
+}
+
+GreySquadronYWing.implemented = true;
