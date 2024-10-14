f968e595e3ed009c11eb99f24b309f8cc149ba78
Valentin Jubert <val_jub@live.fr>
2024-10-14 18:13:32 +0200

    move benthic to ts

--- test/server/cards/01_SOR/units/BenthicTwoTubes.spec.ts
+++ test/server/cards/01_SOR/units/BenthicTwoTubes.spec.ts
@@ -1,8 +1,8 @@
 describe('Benthic Two Tubes', function() {
-    integration(function() {
+    integration(function(contextRef) {
         describe('Benthic Two Tubes\'s ability', function() {
             beforeEach(function () {
-                this.setupTest({
+                contextRef.setupTest({
                     phase: 'action',
                     player1: {
                         groundArena: ['benthic-two-tubes#partisan-lieutenant', 'battlefield-marine'],
@@ -15,14 +15,15 @@ describe('Benthic Two Tubes', function() {
             });
 
             it('should give Raid 2 to another Aggression ally', function () {
-                this.player1.clickCard(this.benthicTwoTubes);
-                this.player1.clickCard(this.p2Base);
+                const { context } = contextRef;
+                context.player1.clickCard(context.benthicTwoTubes);
+                context.player1.clickCard(context.p2Base);
                 // a wing is automatically chosen
-                this.player2.passAction();
+                context.player2.passAction();
 
-                this.player1.clickCard(this.greenSquadronAwing);
+                context.player1.clickCard(context.greenSquadronAwing);
                 // benthic: 2 + a wing: 3+2
-                expect(this.p2Base.damage).toBe(7);
+                expect(context.p2Base.damage).toBe(7);
             });
         });
     });
