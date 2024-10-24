543752bc6462ea8caa6b4240381c43f8ad43094a
Garrett Chesnut <garrettdc2@gmail.com>
2024-10-24 14:06:28 -0500

    Initial implementation of 97th Legion
    Implements 97th legion in a broken state. Currently resource, when derived from the card class itself, is always empty.

---
+++ server/game/cards/01_SOR/units/97thLegionKeepingThePeaceOnSullust.ts
@@ -0,0 +1,20 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class _97thLegionKeepingThePeaceOnSullust extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '7648077180',
+            internalName: '97th-legion#keeping-the-peace-on-sullust',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addConstantAbility({
+            title: 'This unit gets +1/+1 for each resource you control',
+            ongoingEffect: AbilityHelper.ongoingEffects.modifyStats({ power: this.controller.resources.length, hp: this.controller.resources.length }),
+        });
+    }
+}
+
+_97thLegionKeepingThePeaceOnSullust.implemented = true;
---
+++ test/server/cards/01_SOR/units/97thLegionKeepingThePeaceOnSullust.spec.ts
@@ -0,0 +1,25 @@
+describe('97th Legion Keeping the Peace on Sullust', function() {
+    integration(function(contextRef) {
+        describe('97th Legion Keeping the Peace on Sullust\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        resources: 10
+                    },
+                    player2: {
+                        groundArena: ['yoda#old-master'],
+                    }
+                });
+            });
+
+            it('should have power and health equal to the number of resources', function () {
+                const { context } = contextRef;
+                context.player1.setGroundArenaUnits(['97th-legion#keeping-the-peace-on-sullust'], ['removed from game']);
+
+                // check board state
+                expect(context._97thLegion.remainingHp).toBe(10);
+            });
+        });
+    });
+});
