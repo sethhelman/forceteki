e9e574cca89fdb9ed2cbf5f3c15b85d13a07c29b
Valentin Jubert <val_jub@live.fr>
2024-10-31 22:23:40 +0100

    devastating gunship

---
+++ test/server/cards/03_TWI/units/DevastatingGunship.spec.ts
@@ -0,0 +1,29 @@
+describe('Devastating Gunship', function () {
+    integration(function (contextRef) {
+        describe('Devastating Gunship\'s ability', function () {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['devastating-gunship'],
+                        groundArena: ['specforce-soldier']
+                    },
+                    player2: {
+                        groundArena: [{ card: 'battlefield-marine', damage: 2 }, 'wampa'],
+                        spaceArena: ['lurking-tie-phantom']
+                    }
+                });
+            });
+
+            it('should not give -1/-1 because no friendly was defeated this phase', function () {
+                const { context } = contextRef;
+                context.player1.clickCard(context.devastatingGunship);
+                expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.lurkingTiePhantom]);
+                expect(context.player1).not.toHaveChooseNoTargetButton();
+                context.player1.clickCard(context.battlefieldMarine);
+                expect(context.player2).toBeActivePlayer();
+                expect(context.battlefieldMarine.location).toBe('discard');
+            });
+        });
+    });
+});
