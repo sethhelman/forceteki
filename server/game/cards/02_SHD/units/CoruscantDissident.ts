946a856299da10014114242738c71f253626d381
sethhelman <s7helman@gmail.com>
2024-11-20 21:51:52 -0500

    Create CoruscantDissident.ts

---
+++ server/game/cards/02_SHD/units/CoruscantDissident.ts
@@ -0,0 +1,28 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { TargetMode, WildcardCardType } from '../../../core/Constants';
+
+export default class CoruscantDissident extends NonLeaderUnitCard{
+    protected override getImplementationId(){
+        return {
+            id: '9115773123'
+            internalName: 'coruscant-dissident' //confirm before submission
+        };//return end
+    }// override end
+
+    public override setupCardAbilities(){
+        this.addOnAttackAbility({
+        title: 'On attack: You may ready a resource',
+        optional: true,
+        when: {
+            onAttackDeclared: (event, context) => event.attack.attacker === context.source
+        },
+        targetResolver: {
+            mode: TargetMode.Select,
+            ['Ready a resource']: AbilityHelper.immediateEffects.readyResources({ amount: 1 })
+        }
+    });
+    }
+}
+
+CoruscantDissident.implemented = true;
