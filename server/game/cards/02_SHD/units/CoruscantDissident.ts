b207abe5d66d5603686c6377628bf5def8e1064f
sethhelman <s7helman@gmail.com>
2024-11-20 22:37:47 -0500

    Update CoruscantDissident.ts

--- server/game/cards/02_SHD/units/CoruscantDissident.ts
+++ server/game/cards/02_SHD/units/CoruscantDissident.ts
@@ -2,26 +2,26 @@ import AbilityHelper from '../../../AbilityHelper';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
 import { TargetMode, WildcardCardType } from '../../../core/Constants';
 
-export default class CoruscantDissident extends NonLeaderUnitCard{
-    protected override getImplementationId(){
+export default class CoruscantDissident extends NonLeaderUnitCard {
+    protected override getImplementationId() {
         return {
-            id: '9115773123'
-            internalName: 'coruscant-dissident' //confirm before submission
-        };//return end
-    }// override end
+            id: '9115773123',
+            internalName: 'coruscant-dissident'
+        };
+    }
 
-    public override setupCardAbilities(){
+    public override setupCardAbilities() {
         this.addOnAttackAbility({
-        title: 'On attack: You may ready a resource',
-        optional: true,
-        when: {
-            onAttackDeclared: (event, context) => event.attack.attacker === context.source
-        },
-        targetResolver: {
-            mode: TargetMode.Select,
-            ['Ready a resource']: AbilityHelper.immediateEffects.readyResources({ amount: 1 })
-        }
-    });
+            title: 'On attack: You may ready a resource',
+            optional: true,
+            when: {
+                onAttackDeclared: (event, context) => event.attack.attacker === context.source
+            },
+            targetResolver: {
+                mode: TargetMode.Select,
+                ['Ready a resource']: AbilityHelper.immediateEffects.readyResources({ amount: 1 })
+            }
+        });
     }
 }
 
